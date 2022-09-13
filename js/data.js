f24c671c21b7df9d074420c0208d65e28af5275b
Eugene Park <epark215@gmail.com>
2022-09-13 19:14:23 +0000

    Moved the js code unrelated to data to the main

--- js/data.js
+++ js/data.js
@@ -6,26 +6,6 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
-var imageURL = document.querySelector('#PhotoURL');
-var image = document.querySelector('img');
-imageURL.addEventListener('input', function (event) {
-  image.src = imageURL.value;
-});
-
-var form = document.querySelector('form');
-var title = document.querySelector('#Title');
-var notes = document.querySelector('#Notes');
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  var codeJournalObject = {};
-  codeJournalObject[title] = title.value;
-  codeJournalObject[image] = imageURL.value;
-  codeJournalObject[notes] = notes.value;
-  image.src = 'images/placeholder-image-square.jpg';
-  codeJournalObject.entryId = data.nextEntryId++;
-  data.entries.unshift(codeJournalObject);
-  form.reset();
-});
 
 window.addEventListener('beforeunload', function (event) {
   var stringifyDataEntries = JSON.stringify(data.entries);
