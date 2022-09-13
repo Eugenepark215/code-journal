a1f574d4653d82397cb82fff1d07a1f0f405260b
Eugene Park <epark215@gmail.com>
2022-09-13 18:47:10 +0000

    Completed issue-1-create-an-entry

--- js/data.js
+++ js/data.js
@@ -6,3 +6,26 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+var imageURL = document.querySelector('#PhotoURL');
+var image = document.querySelector('img');
+imageURL.addEventListener('input', function (event) {
+  image.src = imageURL.value;
+});
+
+var form = document.querySelector('form');
+var title = document.querySelector('#Title');
+var notes = document.querySelector('#Notes');
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var codeJournalObject = {};
+  codeJournalObject[title] = title.value;
+  codeJournalObject[image] = imageURL.value;
+  codeJournalObject[notes] = notes.value;
+  image.src = 'images/placeholder-image-square.jpg';
+  codeJournalObject[data.nextEntryId] = data.nextEntryId++;
+  data.entries.unshift(codeJournalObject);
+  form.reset();
+
+  var stringifyDataEntries = JSON.stringify(data.entries);
+  localStorage.setItem('code-journal-local-storage', stringifyDataEntries);
+});
